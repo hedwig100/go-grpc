bc8b4b1b14311a92a2bc586928b137e6b926c1fc
hedwig100 <pentagon7951@outlook.jp>
2023-01-01 21:51:06 +0900

    [add] status

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -13,8 +13,11 @@ import (
 
 	hellopb "mygrpc/pkg/grpc"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type myServer struct {
@@ -22,9 +25,16 @@ type myServer struct {
 }
 
 func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
-	return &hellopb.HelloResponse{
-		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
-	}, nil
+	// 正常な場合のコード
+	// return &hellopb.HelloResponse{
+	// 	Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	// }, nil
+	stat := status.New(codes.Unknown, "unknown error occured")
+	stat, _ = stat.WithDetails(&errdetails.DebugInfo{
+		Detail: "deail reason of err",
+	})
+	err := stat.Err()
+	return nil, err
 }
 
 func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
